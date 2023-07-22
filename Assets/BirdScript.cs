9f4214098ae4053be6ba34ac40fa60fe867be5bf
Nikolai Kleppe <nikolai__92@hotmail.com>
2023-07-23 01:27:16 +0200

    add pipes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour {
 
     public Rigidbody2D myRigidbody;
+    public float flapStrength = 6;
+    public float gravityScale = 4;
 
     // Start is called before the first frame update
     void Start() {
         gameObject.name = "GrimLumberjack";
+        myRigidbody.gravityScale = gravityScale;
     }
 
     // Update is called once per frame
     void Update() {
+
+
+        if (Input.GetKeyDown(KeyCode.Space) == true) {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+
+
 
     }
 
