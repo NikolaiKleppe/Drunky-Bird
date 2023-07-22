9766326aaaf2979b339ac41d7513cd0b512cce91
Nikolai Kleppe <nikolai__92@hotmail.com>
2023-07-23 00:20:03 +0200

    updates

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdScript : MonoBehaviour
-{
+public class BirdScript : MonoBehaviour {
 
     public Rigidbody2D myRigidbody;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         gameObject.name = "GrimLumberjack";
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
+
+
+
+
 }
